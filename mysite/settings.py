ccec46c2b9bfd9262143851ebcd46df2d0817ce5
Alex <Alexandrastacey@4261811f904c>
2019-04-27 15:39:14 +0000

    local-changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r4ms%k+(zij5%9*t)ogk@gu2supqz^vf)mu7x_wh=uf0%$a*#b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alexandrastacey.pythonanywhere.com']
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
